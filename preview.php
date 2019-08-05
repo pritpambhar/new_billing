cfb76c75c1df58c828dbdbc17aaf8a7911c1db0d
pritpambhar <pritpambhar@gmail.com>
2019-08-05 23:51:33 +0530

    cancel bill generation solved

--- preview.php
+++ preview.php
@@ -103,14 +103,22 @@
             
             if($cancel_transaction2 && $cancel_transaction1)
             {
-                header("location:index.php");
+                //header("location:index.php");
+                echo '<script type="text/javascript">';
+                echo 'window.location.href="index.php";';
+                echo '</script>';
+                echo '<noscript>';
+                echo '<meta http-equiv="refresh" content="0;url=index.php" />';
+                echo '</noscript>'; 
+                exit;
             }
             else
             {
                 echo "<h2>failed to cancel</h2>";
             }
         }
-        ob_end_flush();?>
+        ob_end_flush();?>   
+
         <script>
             function printt()
             {
